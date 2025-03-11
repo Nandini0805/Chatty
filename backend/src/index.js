f14d5044e0ed5918ef52ae30a0f3e794ac1cc471
Nandini0805 <nandinigupta068@gmail.com>
2025-03-12 01:17:35 +0530

    Project Completed

---
+++ backend/src/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import bodyParser from 'body-parser'; 
+import { app,server } from "./lib/socket.js";
+
+dotenv.config();
+console.log("Mongo URI:", process.env.MONGO);
+connectDB();
+    
+
+app.use(bodyParser.json({ limit: '10mb' })); // Increase the limit for JSON payloads
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true })); // Increase the limit for URL-encoded payloads
+
+// dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+app.use("/api/auth",authRoutes);
+app.use("/api/messages",messageRoutes);
+
+
+server.listen(PORT , () => {
+    console.log("server is running on PORT :" + PORT);
+    connectDB();
+});
+
