dded59694fd1ca65855c727ed82ab90851490a6c
Nandini0805 <153186356+Nandini0805@users.noreply.github.com>
2025-03-12 21:59:23 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
+import path from "path";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -18,6 +20,7 @@ app.use(bodyParser.urlencoded({ limit: '10mb', extended: true })); // Increase t
 
 // dotenv.config();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -28,6 +31,13 @@ app.use(cors({
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 
+if(process.env.NODE_ENV ==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+    });
+}
 
 server.listen(PORT , () => {
     console.log("server is running on PORT :" + PORT);
